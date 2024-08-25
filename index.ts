f4c93068c8da221f5308cabb67463579eb85ec72
RobertsGroza <roberts.groza23@gmail.com>
2024-08-25 21:25:00 +0300

    Basic GET & email functionality

--- index.ts
+++ index.ts
@@ -1,3 +1,64 @@
-const test = "aaa";
+import axios from "axios";
+import type { AxiosResponse, AxiosError } from "axios";
+import { XMLParser } from "fast-xml-parser";
+import { createTransport } from "nodemailer";
 
-console.log("Test: ", test + 3);
+import "dotenv/config";
+
+// TODO: Check if all models are always kebab case (e.g. Alfa Romeo => alfa-romeo)
+const filters = [
+    ["Golf 7", "transport/cars/volkswagen/golf-7"]
+]
+
+const transporter = createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.email,
+        pass: process.env.password
+    }
+})
+
+const mailOptions = {
+    from: process.env.email,
+    to: process.env.my_email,
+    subject: "Market Buddy",
+    text: 'This is a test email sent from a Node.js app!'
+}
+
+const parser = new XMLParser();
+
+interface SSResponse {
+    rss: {
+        channel: {
+            item: SSItem[]
+        }
+    }
+}
+
+type SSItem = {
+    title: string;
+    link: string;
+    pubDate: string;
+    description: string;
+}
+
+axios.get(`https://www.ss.lv/lv/${filters[0][1]}/rss/`)
+    .then((response: AxiosResponse<string>) => {
+        const data = response.data;
+        const result: SSResponse = parser.parse(data);
+        const items = result.rss.channel.item;
+        for(let item of items) {
+            console.log("The car: ", item);
+        }
+        console.log("email? ", process.env.email);
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        })
+    })
+    .catch((err: AxiosError) => console.error(err));
+ 
--- todo.md
+++ todo.md
@@ -1,10 +1,12 @@
 TODO:
 
-- [ ] SS.COM RSS client
-- [ ] Filter for SS XML
+- [x] SS.COM RSS client
+- [ ] Filter for SS
 - [ ] Logic for filtering out duplicates
-- [ ] Email service that sends out results
+- [x] Email service that sends out results
+- [ ] Format email & make it cool
 - [ ] CRON job
 - [ ] CITY24 client
 - [ ] Way to remember last sent advertisement (maybe SQLite)
 - [ ] Think of way how to store filters & apply them
+- [ ] Some sort of user interface?
